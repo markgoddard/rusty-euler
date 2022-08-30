762b81ff8b3853293697a952f09df750c261c5a5
Mark Goddard <mark@stackhpc.com>
2022-08-30 20:21:25 +0100

    Problem 1, with better use of iterators

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,10 @@
 fn problem1() {
     // If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
     // Find the sum of all the multiples of 3 or 5 below 1000.
-    let mut m3 = Vec::new();
-    let mut m5 = Vec::new();
-    let mut m15 = Vec::new();
-    for i in (3..1000).step_by(3) {
-        m3.push(i)
-    }
-    for i in (5..1000).step_by(5) {
-        m5.push(i)
-    }
-    for i in (15..1000).step_by(15) {
-        m15.push(i)
-    }
-    let result: i32 = m3.iter().sum::<i32>() + m5.iter().sum::<i32>() - m15.iter().sum::<i32>();
+    let m3: u32 = (3..1000).step_by(3).sum();
+    let m5: u32 = (5..1000).step_by(5).sum();
+    let m15: u32 = (15..1000).step_by(15).sum();
+    let result = m3 + m5 - m15;
     println!("{result}")
 }
 
