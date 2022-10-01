487bef812420110ce068df015c494f4201d1bda3
Mark Goddard <mark@stackhpc.com>
2022-10-01 21:07:49 +0100

    Problem 8

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::cmp;
+
 /// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 /// Find the sum of all the multiples of 3 or 5 below 1000.
 fn problem1() {
@@ -141,6 +143,41 @@ fn problem7() {
     println!("Problem 7: {result}")
 }
 
+// The four adjacent digits in the 1000-digit number that have the greatest product are 9 × 9 × 8 × 9 = 5832.
+// Find the thirteen adjacent digits in the 1000-digit number that have the greatest product. What is the value of this product?
+fn problem8() {
+    let n = "
+73167176531330624919225119674426574742355349194934
+96983520312774506326239578318016984801869478851843
+85861560789112949495459501737958331952853208805511
+12540698747158523863050715693290963295227443043557
+66896648950445244523161731856403098711121722383113
+62229893423380308135336276614282806444486645238749
+30358907296290491560440772390713810515859307960866
+70172427121883998797908792274921901699720888093776
+65727333001053367881220235421809751254540594752243
+52584907711670556013604839586446706324415722155397
+53697817977846174064955149290862569321978468622482
+83972241375657056057490261407972968652414535100474
+82166370484403199890008895243450658541227588666881
+16427171479924442928230863465674813919123162824586
+17866458359124566529476545682848912883142607690042
+24219022671055626321111109370544217506941658960408
+07198403850962455444362981230987879927244284909188
+84580156166097919133875499200524063689912560717606
+05886116467109405077541002256983155200055935729725
+71636269561882670428252483600823257530420752963450";
+    let n: Vec::<u64> = n.chars().filter(|c| c.is_digit(10)).map(|c| u64::from(c.to_digit(10).unwrap())).collect();
+    let mut max_product = 1;
+    for i in 0..(n.len() - 13) {
+        let adjacents = &n[i..i+13];
+        let product: u64 = adjacents.iter().product();
+        max_product = cmp::max(product, max_product);
+    }
+    let result = max_product;
+    println!("Problem 8: {result}")
+}
+
 pub fn solve_problem(problem: u32) -> Result<(), &'static str> {
     match problem {
         1 => problem1(),
@@ -150,6 +187,7 @@ pub fn solve_problem(problem: u32) -> Result<(), &'static str> {
         5 => problem5(),
         6 => problem6(),
         7 => problem7(),
+        8 => problem8(),
         _ => {
             return Err("Unexpected problem number")
         },
